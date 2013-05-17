d0d841099c4de7fba28a6ec78ea0b59f6ce1beba
James Lee <jlee@thestaticvoid.com>
2013-05-17 14:28:48 -0400

    Initial argparse

--- src/edu/it/duplog/Main.java
+++ src/edu/it/duplog/Main.java
@@ -1,22 +1,30 @@
 package edu.umd.it.duplog;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Subparser;
+import net.sourceforge.argparse4j.inf.Subparsers;
+
 public class Main {
-    private static void usage() {
-        System.err.println("Usage: duplog.jar <send|receive>");
-        System.exit(1);
-    }
-
     public static void main(String[] args) {
-        if (args.length != 1) {
-            usage();
-        }
+        ArgumentParser parser = ArgumentParsers.newArgumentParser("duplog.jar").version("Duplog 1");
+        parser.addArgument("--version").action(Arguments.version());
+        Subparsers subparsers = parser.addSubparsers();
 
-        if (args[0].equals("send")) {
-            Send.send();
-        } else if (args[0].equals("receive")) {
-            Recv.recv(new String[]{"foo1", "foo2"});
-        } else {
-            usage();
+        Subparser sendParser = subparsers.addParser("send").help("read log messages from stdin");
+
+        Subparser receiveParser = subparsers.addParser("receive").help("extract deduplicated log messages").defaultHelp(true);
+        receiveParser.addArgument("-o").dest("output_file").setDefault("/var/log/duplog.log").help("where to write deduplicated log messages");
+        receiveParser.addArgument("-r").dest("redis_server").setDefault("localhost").help("hostname of the Redis server");
+        receiveParser.addArgument("").dest("syslog_server").nargs("+").help("hostname of a syslog server running RabbitMQ");
+
+        try {
+            System.out.println(parser.parseArgs(args));
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
         }
     }
 }
